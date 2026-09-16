046d2b2d9e9cd2dc74c2f2d77207203e322e8e28
thetaung <thetaungheinh@gmail.com>
2026-09-16 11:29:09 +0630

    Created mongo-integration and update the pom.xml and app.java files

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,7 +1,31 @@
 package com.napier.sem;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
+
 public class App {
-    public static void main(String[] args){
-        System.out.println("Booo yaa");
+    static void main() {
+        // Connect using try-with-resources to automatically close MongoClient
+        try (MongoClient mongoClient = new MongoClient("localhost", 27000)) {
+            // Get a database - will create when we use it
+            MongoDatabase database = mongoClient.getDatabase("mydb");
+            // Get a collection from the database
+            MongoCollection<Document> collection = database.getCollection("test");
+            // Create a document to store
+            Document doc = new Document("name", "Kevin Sim")
+                    .append("class", "DevOps")
+                    .append("year", "2024")
+                    .append("result", new Document("CW", 95).append("EX", 85));
+            // Add document to collection
+            collection.insertOne(doc);
+
+            // Check document in collection
+            Document myDoc = collection.find().first();
+            if (myDoc != null) {
+                System.out.println(myDoc.toJson());
+            }
+        }
     }
 }
